f19ad856f4a6e1607f7a44f11d72b918fe426cd3
Tim Beattie <trbeattie@outlook.com>
2020-07-28 16:25:14 -0400

    Add documentation for formatHex() in util.js

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,11 @@
+/**
+ * Returns a formated string representing a hexidecimal number.
+ * 
+ * @param value - The number to be formated 
+ * @param bytes - The minimum number of bytes that should be represented in
+ * the string. This is used for zero-padding.
+ * 
+ */
 export function formatHex(value : number, bytes : number) : string {
 
     return value

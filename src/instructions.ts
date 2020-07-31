3b577e9f983b1edb7859a1d711539c678b86b837
Tim Beattie <trbeattie@outlook.com>
2020-07-31 13:05:34 -0400

    Add/improve documentation in instructions.ts and util.js

--- src/instructions.ts
+++ src/instructions.ts
@@ -1,6 +1,14 @@
 import { formatHex } from './util';
 import { decodedInstruction } from './interfaces';
 
+/**
+ * Returns a formatted line of assembly code
+ * 
+ * @param ProgramCounter - Current position in the program
+ * @param hexValues - A numerical opcode (and optional parameters)
+ * @param instruction - A corresponding decoded assembler instruction
+ * 
+ */
 function formatInstructionLine(
     ProgramCounter : number,
     hexValues : Uint8Array,
@@ -21,11 +29,19 @@ function formatInstructionLine(
 
 }
 
+/**
+ * Returns a decodedInstruction corresponding to the 'STA zpg' instruction
+ * 
+ * @param programCounter - Current position in the program
+ * @param operand - The zeropage address provided to the instruction
+ * 
+ */
 export function STA_zpg(programCounter : number, operand: number) : decodedInstruction {
 
     let instruction = `STA $${formatHex(operand, 1)}`;
     const instructionValue = 0x85;
     let hexValues = new Uint8Array([instructionValue, operand]);
+
     return {
         instruction: formatInstructionLine(programCounter, hexValues, instruction),
         opbytes: 2
--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
 /**
- * Returns a formated string representing a hexidecimal number.
+ * Returns a formatted string representing a hexadecimal number.
  * 
- * @param value - The number to be formated 
+ * @param value - The number to be formatted 
  * @param bytes - The minimum number of bytes that should be represented in
  * the string. This is used for zero-padding.
  * 

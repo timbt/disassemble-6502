540ba0a0699c0d9a57b00747cfd7d3fc4c42d743
Tim Beattie <trbeattie@outlook.com>
2020-07-31 12:31:46 -0400

    Document decodedInstruction interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,4 +1,10 @@
+/** A single decoded assembler instruction */
 export interface decodedInstruction {
+
+    /** The instruction, represented as a single line of assembly code */
     instruction: string,
+
+    /** The number of bytes used by the instruction */
     opbytes: number
+
 };
